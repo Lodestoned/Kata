3b3864befbb659c2a2fd8c71f902ddf35561261a
Lodestoned <32818153+Lodestoned@users.noreply.github.com>
2023-06-15 20:07:54 +0100

    Update From_Roman_Numerals.java

--- Kata_RomanNumerals/From_Roman_Numerals.java
+++ Kata_RomanNumerals/From_Roman_Numerals.java
@@ -1,2 +1,37 @@
+import java.util.HashMap;
+
 public class From_Roman_Numerals{
+
+  public static void main(String[] args){
+
+    HashMap<String, Integer> rValues = new HashMap<String, Integer>();
+    rValues.put("I", 1);
+    rValues.put("V", 5);
+    rValues.put("X", 10);
+    rValues.put("L", 50);
+
+    String[] inputValue = null;
+    int inputLength = 0;
+
+    // Check input #Should pass this through a REGEX check method.
+    if(args.length > 0){
+      inputValue = args[0].split("(?!^)");
+      inputLength = inputValue.length;
+    }else{
+      // if no Roman Numeral was given
+      System.exit(0);
+    }
+
+    int[] instruction = new int[inputLength];
+
+    if(inputLength > 1){
+      for (int i=0; i<inputLength; i++ ) {
+        //if(){
+
+        //}
+      }
+    }else{
+      System.out.print(rValues.get(inputValue[0]));
+    }
+  }
 }
