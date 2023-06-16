dfeda91cdc054d8b8a23d824d1311f3423838bd3
Lodestoned <32818153+Lodestoned@users.noreply.github.com>
2023-06-16 17:37:13 +0100

    Update From_Roman_Numerals.java

--- Kata_RomanNumerals/From_Roman_Numerals.java
+++ Kata_RomanNumerals/From_Roman_Numerals.java
@@ -12,6 +12,7 @@ public class From_Roman_Numerals{
 
     String[] inputValue = null;
     int inputLength = 0;
+    int output = 0;
 
     // Check input #Should pass this through a REGEX check method.
     if(args.length > 0){
@@ -25,11 +26,24 @@ public class From_Roman_Numerals{
     int[] instruction = new int[inputLength];
 
     if(inputLength > 1){
-      for (int i=0; i<inputLength; i++ ) {
-        //if(){
-
-        //}
+      for (int i=1; i<inputLength; i++ ) {
+        if(rValues.get(inputValue[i-1]) < rValues.get(inputValue[i]) ){
+          instruction[i] = -1;
+        }
       }
+
+      for(int i=inputLength -1; i>=0; i--){
+
+        output += rValues.get(inputValue[i]);
+
+        if(instruction[i] == -1){
+          output -= rValues.get(inputValue[i-1]);
+          i = i-1;
+        }
+      }
+
+      System.out.print(output);
+
     }else{
       System.out.print(rValues.get(inputValue[0]));
     }
