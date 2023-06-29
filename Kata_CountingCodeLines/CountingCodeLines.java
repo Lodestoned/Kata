3037e4862d43a2f15fc7ab2a058e189c8dbc4454
Lodestoned <32818153+Lodestoned@users.noreply.github.com>
2023-06-29 20:38:57 +0100

    Update CountingCodeLines.java

--- Kata_CountingCodeLines/CountingCodeLines.java
+++ Kata_CountingCodeLines/CountingCodeLines.java
@@ -2,16 +2,25 @@ public class CountingCodeLines {
 
   public static void main(String[] args){
     //TODO: check file is valid, if valid open file and check each line adding lines with code to counter. output count to screen.
+    if(validateFileType(args[0])){
+      System.out.print("it worked");
+    }
 
   }
 
-  private boolean validateFileType(String filename){
-    //TODO: implement Regex check
-    return true;
+  private static boolean validateFileType(String filename){
+    // All files must be .java files and cannot have special characters.
+    String pattern = "[a-zA-Z0-9]{1,}\\.java";
+
+    if(filename.matches(pattern)){
+      return true;
+    }
+    // else
+    return false;
   }
 
-  private boolean checkLineForCode(String lineFromFile){
+  private static boolean checkLineForCode(String lineFromFile){
     //TODO: ipmlement Regex check
-    return false
+    return false;
   }
 }
