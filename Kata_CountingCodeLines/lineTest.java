7395c1f0934a3943d03bec95f714a131559b7cb8
Lodestoned <32818153+Lodestoned@users.noreply.github.com>
2023-07-07 22:47:33 +0100

    Add files via upload

---
+++ Kata_CountingCodeLines/lineTest.java
@@ -0,0 +1,11 @@
+public class lineTest{
+  public static void main(String[] args){
+    System.out.print("Hello World!"); // another trick comment
+
+    /// This program should contain 5 lines. comments and blank spaces should not be counted.
+
+    /*
+    here is a trick comment
+    */
+  }
+}
