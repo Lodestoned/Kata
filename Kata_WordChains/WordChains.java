460a31426f98406b76313a8603bfab154de478ea
Lodestoned <32818153+Lodestoned@users.noreply.github.com>
2023-07-09 17:45:24 +0100

    Add files via upload

---
+++ Kata_WordChains/WordChains.java
@@ -0,0 +1,14 @@
+public class WordChains{
+  ///quicksort idea, make list of words and order them, then check for farthest away from start with only one change. (nearest to goal word)
+  ///A* idea
+  ///
+  public static void main(String[] args){
+
+  }
+
+  private static boolean checkFiletype(String filePath){
+    ///TODO: add regex check
+    return false
+  }
+
+}
---
+++ Kata_WordChains/WordChainsAStar.java
@@ -0,0 +1,5 @@
+public class WordChainsAStar{
+///quicksort idea, make list of words and order them, then check for farthest away from start with only one change. (nearest to goal word)
+///A* idea
+///
+}
