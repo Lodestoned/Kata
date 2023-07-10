1f8a7450f9e2fa43662579f84cb6d18eda72d5db
Lodestoned <32818153+Lodestoned@users.noreply.github.com>
2023-07-10 21:16:32 +0100

    Update WordChains.java

--- Kata_WordChains/WordChains.java
+++ Kata_WordChains/WordChains.java
@@ -1,14 +1,37 @@
+import java.util.ArrayList; // import the ArrayList class
+import java.io.*;
+import java.util.Scanner;
+
+
 public class WordChains{
   ///quicksort idea, make list of words and order them, then check for farthest away from start with only one change. (nearest to goal word)
-  ///A* idea
-  ///
   public static void main(String[] args){
+    ArrayList<String> wordlist = new ArrayList<String>();
+
+    // String[] lineOfWords = {"Hi", "Buy", "bye"};
+    // wordlist.add("test");
+    // java.util.Collections.addAll(wordlist, lineOfWords);
 
   }
 
   private static boolean checkFiletype(String filePath){
-    ///TODO: add regex check
-    return false
+    String pattern = "[a-zA-Z0-9]{1,}\\.txt";
+    String pattern2 = "[a-zA-Z0-9]{1,}\\.csv";
+
+    if(filePath.matches(pattern) || filePath.matches(pattern2)){
+      return true;
+    }
+    return false;
   }
 
+  private static void /* ArrayList<String> */ getfile(String filePath){
+    try{
+      File inputFile = new File(filePath);
+      Scanner in = new Scanner(inputFile);
+
+    }catch(IOException exception){
+      System.out.println(exception);
+    }
+    /// TODO: remember to close Scanner
+  }
 }
