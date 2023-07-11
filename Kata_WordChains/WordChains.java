e600b1137341795564de146b98e6b00263be9e5d
Lodestoned <32818153+Lodestoned@users.noreply.github.com>
2023-07-11 19:43:59 +0100

    Update WordChains.java

--- Kata_WordChains/WordChains.java
+++ Kata_WordChains/WordChains.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList; // import the ArrayList class
+import java.util.ArrayList;
 import java.io.*;
 import java.util.Scanner;
 
@@ -6,17 +6,28 @@ import java.util.Scanner;
 public class WordChains{
   ///quicksort idea, make list of words and order them, then check for farthest away from start with only one change. (nearest to goal word)
   public static void main(String[] args){
-    ArrayList<String> wordlist = new ArrayList<String>();
+    ArrayList<String> wordList = null;
 
-    // String[] lineOfWords = {"Hi", "Buy", "bye"};
-    // wordlist.add("test");
-    // java.util.Collections.addAll(wordlist, lineOfWords);
+    if(args.length != 3){
+      System.out.println("Missing required parameters: dictionary, starting word, goal word.");
+      System.exit(0);
+    }
+
+    if(checkFiletype(args[0])){
+      wordList = getCustomDictionary(args[0]);
+
+      /// TODO: make logic to find words in list. and fill connecting words.
+      System.out.println(wordList.size());
+
+    }else{
+      System.out.println("Incorrect Filetype.");
+    }
 
   }
 
   private static boolean checkFiletype(String filePath){
-    String pattern = "[a-zA-Z0-9]{1,}\\.txt";
-    String pattern2 = "[a-zA-Z0-9]{1,}\\.csv";
+    String pattern = "[a-zA-Z0-9\\_\\-]{1,}\\.txt";
+    String pattern2 = "[a-zA-Z0-9\\_\\-]{1,}\\.csv";
 
     if(filePath.matches(pattern) || filePath.matches(pattern2)){
       return true;
@@ -24,14 +35,30 @@ public class WordChains{
     return false;
   }
 
-  private static void /* ArrayList<String> */ getfile(String filePath){
-    try{
-      File inputFile = new File(filePath);
-      Scanner in = new Scanner(inputFile);
+  private static ArrayList<String> getCustomDictionary(String filePath){
 
+    ArrayList<String> wordList = new ArrayList<String>();
+    File inputFile = null;
+    Scanner in = null;
+
+    try{
+      inputFile = new File(filePath);
+
+      if(!inputFile.exists()){
+        System.out.println("File not found");
+      }else{
+        in = new Scanner(inputFile);
+
+        while(in.hasNext()){
+          java.util.Collections.addAll(wordList, in.nextLine().split(","));
+        }
+        // close Scanner
+        in.close();
+      }
     }catch(IOException exception){
       System.out.println(exception);
     }
-    /// TODO: remember to close Scanner
+
+    return wordList;
   }
 }
